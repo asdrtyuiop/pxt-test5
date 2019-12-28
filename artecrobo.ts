6cdbf60ea5a6567ac76504ab24dd290120231239
asdrtyuiop <44797929+asdrtyuiop@users.noreply.github.com>
2019-12-28 21:24:15 +0800

    Add files via upload

--- artecrobo.ts
+++ artecrobo.ts
@@ -1,22 +1,32 @@
 /**
  * Types of DC motor control
  */
-enum LEDSensorSmotion {
-	//% block="On"
-	On,
-	//% block="Off"
-	Off
+enum DCmotion {
+	//% block="Forward"
+	Forward,
+	//% block="Backward"
+	Backward,
+	//% block="Brake"
+	Brake,
+	//% block="Coast"
+	Coast
 }
 
-enum connectorLEDSensor {
-	//% block="P0"
-	P0,
-	//% block="P1"
-	P1,
-	//% block="P2"
-	P2
+enum connectorDCMotor {
+	//% block="M1"
+	M1,
+	//% block="M2"
+	M2
 }
 
+enum connectorServoMotor {
+	//% block="P13"
+	P13 = AnalogPin.P13,
+	//% block="P14"
+	P14 = AnalogPin.P14,
+	//% block="P15"
+	P15 = AnalogPin.P15
+}
 
 /**
  * ArtecRobo control package
@@ -25,27 +35,72 @@ enum connectorLEDSensor {
 namespace artecrobo {
 
 	/* spped initial value */
-	
-	
+	let speedM1 = 1023;
+	let speedM2 = 1023;
+	let state = DCmotion.Brake;
 
 	// Move DC motor
 	//% blockId=artec_move_dc_motor
-	//% block="LED %_connector| : %_motion"
-	export function LEDSensor(_connector: connectorLEDSensor, _motion: LEDSensormotion): void {
+	//% block="DC motor %_connector| motion: %_motion"
+	export function moveDCMotor(_connector: connectorDCMotor, _motion: DCmotion): void {
 		switch(_motion) {
-			case LEDSensormotion.On:
-				
-				pins.digitalWritePin(DigitalPin._connector, 1);
-
-				
+			case DCmotion.Forward:
+				/*
+					Move Forward
+					M1:P8 = speed, P12 = 0
+					M2:P0 = speed, P16 = 0
+				*/
+				if (_connector == connectorDCMotor.M1) {
+					pins.digitalWritePin(DigitalPin.P8, 1);
+					pins.analogWritePin(AnalogPin.P12, speedM1);
+				} else {
+					pins.digitalWritePin(DigitalPin.P0, 1);
+					pins.analogWritePin(AnalogPin.P16, speedM2);
+				}
 				break;
-			case LEDSensormotion.Off:
-				
-				pins.digitalWritePin(DigitalPin._connector, 0);
+			case DCmotion.Backward:
+				/*
+					Move Backward
+					M1:P8 = 0, P12 = speeed
+					M2:P0 = 0, P16 = speeed
+				*/
+				if (_connector == connectorDCMotor.M1) {
+					pins.analogWritePin(AnalogPin.P8, speedM1);
+					pins.digitalWritePin(DigitalPin.P12, 1);
+				} else {
+					pins.analogWritePin(AnalogPin.P0, speedM2);
+					pins.digitalWritePin(DigitalPin.P16, 1);
+				}
+				break;
+			case DCmotion.Brake:
+				/*
+					Brake
+					M1:P8 = 1, P12 = 1
+					M2:P0 = 1, P16 = 1
+				*/
+				if (_connector == connectorDCMotor.M1) {
+					pins.digitalWritePin(DigitalPin.P8, 1);
+					pins.digitalWritePin(DigitalPin.P12, 1);
+				} else {
+					pins.digitalWritePin(DigitalPin.P0, 1);
+					pins.digitalWritePin(DigitalPin.P16, 1);
+				}
+				break;
+			case DCmotion.Coast:
+				/*
+					Coast
+					M1:P8 = 0, P12 = 0
+					M2:P0 = 0, P16 = 0
+				*/
+				if (_connector == connectorDCMotor.M1) {
+					pins.digitalWritePin(DigitalPin.P8, 0);
+					pins.digitalWritePin(DigitalPin.P12, 0);
+				} else {
+					pins.digitalWritePin(DigitalPin.P0, 0);
+					pins.digitalWritePin(DigitalPin.P16, 0);
+				}
 				break;
 		}
-
+		state = _motion;
 	}
-	
-
 }
